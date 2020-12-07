c2b30e9d9b8c4812c0e5dd9d596bfecb37164b94
junhyun <pe.jeon@gmail.com>
2020-12-07 21:30:02 +0900

    tmp

--- lib/widgets/alarm/expenseImageDialog.dart
+++ lib/widgets/alarm/expenseImageDialog.dart
@@ -9,8 +9,6 @@ ExpenseImageDialog(BuildContext context, String imageUrl) {
   Image image = Image.network(imageUrl);
   double _width;
   double _height;
-  double _rWidth;
-  double _rHeight;
   double _cWidth = customWidth(context: context, widthSize: 0.7);
   double _cHeight = customHeight(context: context, heightSize: 0.7);
 
@@ -30,6 +28,7 @@ ExpenseImageDialog(BuildContext context, String imageUrl) {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
+            elevation: 0.0,
             backgroundColor: Colors.transparent,
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0)),
@@ -41,93 +40,56 @@ ExpenseImageDialog(BuildContext context, String imageUrl) {
                     ui.Image _image = snapshot.data;
                     _width = _image.width.toDouble();
                     _height = _image.height.toDouble();
-
-                    double tmp;
-                    /// 위젯 사이즈 조정
-                    if (_width <= _cWidth && _height <= _cHeight) {
-                      debugPrint("Width and Height is Under");
-                      if(_height/_width >= 1.0) {
-                        _rHeight = _cHeight;
-                        _rWidth = _width * _height/_width;
-                      } else {
-                        _rWidth = _cWidth;
-                        _rHeight = _height * _cWidth/_width;
-                      }
-                    } else if (_width <= _cWidth || _height <= _cHeight) {
-                      debugPrint("Width or Height is Over");
-                      if(_height/_width >= 1.0) {
-                        _rHeight = _cHeight;
-                        _rWidth = _width * _cHeight/_height;
-                      } else {
-                        _rWidth = _cWidth;
-                        _rHeight = _height * _cWidth/_width;
-                      }
-                    } else {
-                      debugPrint("Width and Height is Over");
-                      if(_height/_width >= 1.0) {
-                        _rHeight = _cHeight;
-                        _rWidth = _width * _cHeight/_height;
-                      } else {
-                        _rWidth = _cWidth;
-                        _rHeight = _height * _cWidth/_width;
-                      }
-                    }
-
-
                     return Container(
                         padding: EdgeInsets.all(0.0),
-                        color: Colors.blue,
-                        width: (_width <= _cWidth ? _width : _cWidth),
-                        height: ((_height <= _cHeight ? _height : _cHeight) + 55.0),
+                        color: Colors.red,
+
                         child: Column(
-                            mainAxisAlignment:
-                                MainAxisAlignment.spaceBetween,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    SizedBox(
-                                      width : _rWidth,
-                                        height: _rHeight,
-                                        /*width: (_width <= _cWidth
-                                            ? _width
-                                            : _cWidth),
-                                        height: ((_height <= _cHeight
-                                            ? _height
-                                            : _cHeight)),*/
-                                        child: PhotoView(
-                                          imageProvider:
-                                              NetworkImage(imageUrl),
-                                          minScale: PhotoViewComputedScale
-                                                  .contained *
-                                              0.8,
-                                          maxScale: PhotoViewComputedScale
-                                                  .covered *
-                                              1.8,
-                                          initialScale:
-                                              PhotoViewComputedScale
-                                                      .contained *
-                                                  1.0,
-                                        )),
+                                    ClipRect(
+                                      child: SizedBox(
+                                          width: (_width <= _cWidth
+                                              ? _width
+                                              : _cWidth),
+                                          height: ((_height <= _cHeight
+                                              ? _height
+                                              : _cHeight)+10),
+                                          child: PhotoView(
+                                            backgroundDecoration: BoxDecoration(
+                                              color: Colors.transparent,
+                                            ),
+                                            imageProvider: NetworkImage(imageUrl),
+                                            minScale:
+                                                PhotoViewComputedScale.contained *
+                                                    0.8,
+                                            maxScale:
+                                                PhotoViewComputedScale.covered *
+                                                    1.8,
+                                            initialScale:
+                                                PhotoViewComputedScale.contained *
+                                                    1.0,
+                                          )),
+                                    ),
                                   ]),
-                              Padding(padding: EdgeInsets.only(top:5.0)),
+                              Padding(padding: EdgeInsets.only(top: 5.0)),
                               Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
                                     SizedBox(
                                       width: 100.0,
-                                      height: 50.0,
+                                      height: 40.0,
                                       child: RaisedButton(
                                         onPressed: () {
                                           Navigator.pop(context);
                                         },
                                         child: Text(
                                           "Save",
-                                          style: TextStyle(
-                                              color: Colors.white),
+                                          style: TextStyle(color: Colors.white),
                                         ),
                                         color: const Color(0xFF1BC0C5),
                                       ),
@@ -137,69 +99,6 @@ ExpenseImageDialog(BuildContext context, String imageUrl) {
                   } else {
                     return Container();
                   }
-                }
-
-                /*child: Container(
-                  height: 300,
-                  width : 200,
-                  child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  FutureBuilder<ui.Image>(
-                                      future: _getImage(),
-                                      builder: (BuildContext context,
-                                          AsyncSnapshot<ui.Image> snapshot) {
-                                        if (snapshot.data != null) {
-                                          ui.Image _image = snapshot.data;
-                                          _width = _image.width.toDouble();
-                                          _height = _image.height.toDouble();
-                                          return SizedBox(
-                                              width: _width <= _cWidth ? _width: _cWidth,
-                                              height: _height <= _cHeight ? _height: _cHeight,
-                                              child: PhotoView(
-                                                imageProvider:
-                                                    NetworkImage(imageUrl),
-                                                minScale: PhotoViewComputedScale
-                                                        .contained *
-                                                    0.8,
-                                                maxScale: PhotoViewComputedScale
-                                                        .covered *
-                                                    1.8,
-                                                initialScale:
-                                                    PhotoViewComputedScale
-                                                            .contained *
-                                                        1.0,
-                                              ));
-                                        } else {
-                                          return Container();
-                                        }
-                                      }
-                                      ),
-                                ]),
-                            Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    width: 100.0,
-                                    child: RaisedButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text(
-                                        "Save",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                      color: const Color(0xFF1BC0C5),
-                                    ),
-                                  ),
-                                ])
-                          ]))),*/
-                ));
+                }));
       });
 }
