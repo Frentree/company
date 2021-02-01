5fe950a644a7ea497664fa225519453ab579a2a3
DESKTOP-779HM91\LYH <pkdlsbgur@naver.com>
2021-02-01 10:45:35 +0900

    a

--- lib/screens/setting/annualLeave.dart
+++ lib/screens/setting/annualLeave.dart
@@ -1,17 +1,12 @@
 import 'package:MyCompany/consts/colorCode.dart';
-import 'package:MyCompany/consts/font.dart';
 import 'package:MyCompany/consts/screenSize/size.dart';
 import 'package:MyCompany/consts/screenSize/style.dart';
-import 'package:MyCompany/consts/widgetSize.dart';
 import 'package:MyCompany/i18n/word.dart';
 import 'package:MyCompany/models/userModel.dart';
 import 'package:MyCompany/models/workModel.dart';
 import 'package:MyCompany/provider/user/loginUserInfo.dart';
-import 'package:MyCompany/repos/firebaseMethod.dart';
 import 'package:MyCompany/repos/firebaseRepository.dart';
-import 'package:MyCompany/widgets/dialog/organizationChartDialogList.dart';
-import 'package:MyCompany/widgets/notImplementedPopup.dart';
-import 'package:MyCompany/widgets/popupMenu/expensePopupMenu.dart';
+import 'package:MyCompany/utils/date/dateFormat.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +14,7 @@ import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 final word = Words();
+double count = 15.0;
 
 class AnnualLeavePage extends StatefulWidget {
   @override
@@ -27,43 +23,141 @@ class AnnualLeavePage extends StatefulWidget {
 
 class _AnnualLeavePageState extends State<AnnualLeavePage> {
   @override
+  User _loginUser;
+  String yearDate = DateTime.now().year.toString();
+  int changeDate = 0;
+
   Widget build(BuildContext context) {
+    count = 15.0;
     LoginUserInfoProvider _loginUserInfoProvider = Provider.of<LoginUserInfoProvider>(context);
     _loginUser = _loginUserInfoProvider.getLoginUser();
-
     return Scaffold(
       backgroundColor: whiteColor,
-      body: _buildBody(context, _loginUser),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios_sharp),
+                  onPressed: () {
+                    setState(() {
+                      changeDate -= 1;
+                      yearDate = (DateTime.now().year + changeDate).toString();
+                    });
+                  },
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(yearDate + " 년", style: defaultMediumStyle),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward_ios_sharp),
+                  onPressed: () {
+                    setState(() {
+                      changeDate += 1;
+                      yearDate = (DateTime.now().year + changeDate).toString();
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: cardPadding,
+            child: Container(
+              height: cardTitleSizeH.h,
+              child: Row(children: [
+                cardSpace,
+                cardSpace,
+                cardSpace,
+                Container(
+                  width: SizerUtil.deviceType == DeviceType.Tablet ? 22.0.w : 20.0.w,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text(
+                        "사용일",
+                        style: cardBlueStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                cardSpace,
+                cardSpace,
+                Container(
+                  width: SizerUtil.deviceType == DeviceType.Tablet ? 21.0.w : 19.0.w,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text(
+                        "종류",
+                        style: cardBlueStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                cardSpace,
+                Container(
+                  width: SizerUtil.deviceType == DeviceType.Tablet ? 15.0.w : 13.0.w,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text(
+                        "차감일",
+                        style: cardBlueStyle,
+                      ),
+                    ],
+                  ),
+                ),
+                cardSpace,
+                Container(
+                  width: SizerUtil.deviceType == DeviceType.Tablet ? 18.0.w : 16.0.w,
+                  alignment: Alignment.center,
+                  child: Text(
+                    "남은일",
+                    style: cardBlueStyle,
+                  ),
+                ),
+                /*Container(
+                      width: SizerUtil.deviceType == DeviceType.Tablet ? 11.0.w : 9.0.w,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "옵션",
+                        style: cardBlueStyle,
+                      ),
+                    ),*/
+              ]),
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseRepository().selectAnnualLeave(mail: _loginUser.mail, companyCode: _loginUser.companyCode, date: yearDate),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return LinearProgressIndicator();
+
+                return _buildList(context, snapshot.data.docs, _loginUser.companyCode);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
-
-  User _loginUser;
-}
-
-Widget _buildBody(BuildContext context, User user) {
-  return StreamBuilder<QuerySnapshot>(
-    stream: FirebaseRepository().selectAnnualLeave(
-      mail: user.mail,
-      companyCode: user.companyCode,
-    ),
-    builder: (context, snapshot) {
-      if (!snapshot.hasData) return LinearProgressIndicator();
-
-      return _buildList(context, snapshot.data.docs, user.companyCode);
-    },
-  );
 }
 
 Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot, String companyCode) {
-  print(snapshot.length.toString());
+  /*return ListView(
+    children: snapshot.map((data) => _buildListItem(context, data, companyCode)).toList(),
+  );*/
   return CustomScrollView(
     slivers: [
       SliverList(
-        delegate: SliverChildBuilderDelegate(
-                (context, index) => _buildListItem(context, snapshot[index], companyCode),
-            childCount: snapshot.length),
+        delegate: SliverChildBuilderDelegate((context, index) => _buildListItem(context, snapshot[index], companyCode), childCount: snapshot.length),
       ),
-
     ],
   );
   /*return ListView.builder(
@@ -76,28 +170,39 @@ Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot, String 
 
 Widget _buildListItem(BuildContext context, DocumentSnapshot data, String companyCode) {
   final work = WorkModel.fromSnapshow(data);
-
+  count = (work.type == "연차") ? count - 1.0 : count - 0.5;
   return Container(
     padding: cardPadding,
-    height: 20.0.h,
-    decoration: BoxDecoration(
-        border: Border(
-            bottom: BorderSide(
-                color: grayColor
-            )
-        )
-    ),
-    child: _buildUserList(context, work, companyCode),
+    decoration: BoxDecoration(border: Border(bottom: BorderSide(color: grayColor))),
+    child: _buildUserList(context, work, companyCode, count),
   );
 }
 
-Widget _buildUserList(BuildContext context, WorkModel work, String companyCode) {
-
-  return StatefulBuilder(
-    builder: (context, setState) {
-     return Text(work.title.toString());
-
-    },
+Widget _buildUserList(BuildContext context, WorkModel work, String companyCode, double count) {
+  Format _format = Format();
+  return Row(
+    children: [
+      Container(
+        width: SizerUtil.deviceType == DeviceType.Tablet ? 26.0.w : 24.0.w,
+        alignment: Alignment.center,
+        child: Text(_format.yearMonthDay(work.startTime), style: defaultMediumStyle),
+      ),
+      cardSpace,
+      cardSpace,
+      cardSpace,
+      Container(
+        width: SizerUtil.deviceType == DeviceType.Tablet ? 22.0.w : 20.0.w,
+        alignment: Alignment.topLeft,
+        child: Text(work.type.toString(), style: defaultMediumStyle),
+      ),
+      cardSpace,
+      Container(
+        width: SizerUtil.deviceType == DeviceType.Tablet ? 16.0.w : 14.0.w,
+        alignment: Alignment.topLeft,
+        child: Text((work.type == "연차" ? "-1" : "-0.5") + "일", style: defaultMediumStyle),
+      ),
+      cardSpace,
+      Text(count.toString() + " 일", style: defaultMediumStyle),
+    ],
   );
-
 }
